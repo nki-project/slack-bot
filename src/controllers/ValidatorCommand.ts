1a413d8c8b10dc382ecb390592f497433e520b0a
PPROGER <akornienko882@gmail.com>
2021-11-07 19:59:19 +0300

    added validate command and refactoring

---
+++ src/controllers/ValidatorCommand.ts
@@ -0,0 +1,29 @@
+import {Command} from "./Command";
+
+
+export class ValidatorCommand {
+
+    static validate(commands: Array<Command>, data: any):any {
+        const splitData = data.split(' ');
+        const [nameCommand, ...properties] = splitData;
+        const [commandSearch] = commands.filter(command => command.name === nameCommand);
+        if(!commandSearch) throw new Error("Command not found!");
+        if(properties.length!==commandSearch.countArgs) throw new Error("Invalid count args!");
+
+        for (let indexType = 0; indexType<commandSearch.typeArgs.length; indexType++) {
+            if(commandSearch.typeArgs[indexType] == 'number' && Number(properties[indexType])) {
+                continue;
+            }else if(commandSearch.typeArgs[indexType] == 'date' && new Date(properties[indexType]).toString() !== 'Invalid Date') {
+                continue;
+            }
+            else if(commandSearch.typeArgs[indexType] == 'string' && String(properties[indexType])) {
+                continue;
+            }else {
+                throw new Error("Invalid type args!");
+            }
+        }
+
+        return commandSearch.initProperties(properties);
+    }
+
+}
