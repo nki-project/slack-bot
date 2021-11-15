d4147ee2bf904682b14c1b9950c3238ac7d02f25
injectionsuccesfully <chopenko.nikita.konkurs@gmail.com>
2021-11-15 22:10:13 +0200

    check local time #6

--- src/model/DateTime.ts
+++ src/model/DateTime.ts
@@ -16,7 +16,7 @@ export default class DateTime {
         } else {
             this.dateTime =  dateTime;
         }
-        this.dateTime = moment(this.dateTime).tz(timeZone).toDate();
+        this.dateTime = moment.utc().tz(timeZone).toDate();
     }
 
     get moment(): Moment {
